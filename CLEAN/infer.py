1273bb7def8c446899c13029a7d3e34f3430f44d
biomg <40063814+biomg@users.noreply.github.com>
2024-07-12 16:34:57 +0800

    Update infer.py

--- CLEAN/infer.py
+++ CLEAN/infer.py
@@ -1,10 +1,11 @@
 import torch
 from .utils import * 
-from .model import LayerNormNet
+from .model import *
 from .distance_map import *
 from .evaluate import *
 import pandas as pd
 import warnings
+from torch.cuda.amp import autocast
 
 def warn(*args, **kwargs):
     pass
@@ -21,8 +22,8 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
     # NOTE: change this to LayerNormNet(512, 256, device, dtype) 
     # and rebuild with [python build.py install]
     # if inferencing on model trained with supconH loss
-    model = LayerNormNet(512, 256, device, dtype)
-    #model = LayerNormNet(512, 128, device, dtype)
+    '''
+    model = CNN3(512, 128, device, dtype)
     
     if pretrained:
         try:
@@ -36,7 +37,13 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
             raise Exception('No model found!')
         
     model.load_state_dict(checkpoint)
-    model.eval()
+    '''
+    #use training inference directly
+    model = model_name
+    
+    
+    with torch.no_grad():
+        model.eval()
     # load precomputed EC cluster center embeddings if possible
     if pretrained:
         if train_data == "split70":
@@ -44,18 +51,22 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
         elif train_data == "split100":
             emb_train = torch.load('./data/pretrained/100.pt', map_location=device)
     else:
-        emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
+        with autocast():
+            emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
         
     emb_test = model_embedding_test(id_ec_test, model, device, dtype)
+
     eval_dist = get_dist_map_test(emb_train, emb_test, ec_id_dict_train, id_ec_test, device, dtype)
     seed_everything()
     eval_df = pd.DataFrame.from_dict(eval_dist)
+
     rand_nk_ids, rand_nk_emb_train = random_nk_model(
         id_ec_train, ec_id_dict_train, emb_train, n=nk_random, weighted=True)
+
     random_nk_dist_map = get_random_nk_dist_map(
         emb_train, rand_nk_emb_train, ec_id_dict_train, rand_nk_ids, device, dtype)
-    ensure_dirs("./results")
-    out_filename = "results/" +  test_data
+    ensure_dirs("./results_esm2")
+    out_filename = "results_esm2/" +  test_data
     write_pvalue_choices( eval_df, out_filename, random_nk_dist_map, p_value=p_value)
     # optionally report prediction precision/recall/...
     if report_metrics:
@@ -64,6 +75,7 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
         true_label, all_label = get_true_labels('./data/' + test_data)
         pre, rec, f1, roc, acc = get_eval_metrics(
             pred_label, pred_probs, true_label, all_label)
+        
         print(f'############ EC calling results using random '
         f'chosen {nk_random}k samples ############')
         print('-' * 75)
@@ -71,6 +83,7 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
             f'>>> precision: {pre:.3} | recall: {rec:.3}'
             f'| F1: {f1:.3} | AUC: {roc:.3} ')
         print('-' * 75)  
+        
     return pre, rec, f1, roc, acc
 
 
@@ -85,8 +98,10 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
     # NOTE: change this to LayerNormNet(512, 256, device, dtype) 
     # and rebuild with [python build.py install]
     # if inferencing on model trained with supconH loss
-    model = LayerNormNet(512, 256, device, dtype)
-    #model = LayerNormNet(512, 128, device, dtype)
+    #model = LayerNormNet(512, 256, device, dtype)
+    
+    '''
+    model = CNN3(512, 128, device, dtype)
     
     if pretrained:
         try:
@@ -100,7 +115,12 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
             raise Exception('No model found!')
             
     model.load_state_dict(checkpoint)
-    model.eval()
+    '''
+    #use training inference directly
+    model = model_name
+    
+    with torch.no_grad():
+        model.eval()
     # load precomputed EC cluster center embeddings if possible
     if pretrained:
         if train_data == "split70":
@@ -108,14 +128,15 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
         elif train_data == "split100":
             emb_train = torch.load('./data/pretrained/100.pt', map_location=device)
     else:
-        emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
+        with autocast():
+            emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
         
     emb_test = model_embedding_test(id_ec_test, model, device, dtype)
     eval_dist = get_dist_map_test(emb_train, emb_test, ec_id_dict_train, id_ec_test, device, dtype)
     seed_everything()
     eval_df = pd.DataFrame.from_dict(eval_dist)
-    ensure_dirs("./results")
-    out_filename = "results/" +  test_data
+    ensure_dirs("./results_esm2")
+    out_filename = "results_esm2/" +  test_data
     write_max_sep_choices(eval_df, out_filename, gmm=gmm)
     if report_metrics:
         pred_label = get_pred_labels(out_filename, pred_type='_maxsep')
@@ -123,10 +144,12 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
         true_label, all_label = get_true_labels('./data/' + test_data)
         pre, rec, f1, roc, acc = get_eval_metrics(
             pred_label, pred_probs, true_label, all_label)
+        
         print("############ EC calling results using maximum separation ############")
         print('-' * 75)
         print(f'>>> total samples: {len(true_label)} | total ec: {len(all_label)} \n'
             f'>>> precision: {pre:.3} | recall: {rec:.3}'
             f'| F1: {f1:.3} | AUC: {roc:.3} ')
         print('-' * 75)
+        
     return pre, rec, f1, roc, acc
