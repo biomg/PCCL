b6eee3ff7af4ff172faa7ac1019e9e87669510c5
biomg <40063814+biomg@users.noreply.github.com>
2024-07-12 16:27:30 +0800

    Update pre-train.py

--- pre-train.py
+++ pre-train.py
@@ -4,14 +4,15 @@ train_file = "split100"
 
 #esm1b
 csv_to_fasta("data/split100.csv", "data/split100.fasta")
-#retrive_esm1b_embedding("split100")
+retrive_esm1b_embedding("split100")
+train_fasta_file = mutate_single_seq_ECs(train_file)
+retrive_esm1b_embedding(train_fasta_file)
+compute_esm_distance(train_file)
 
-
-#train_fasta_file = mutate_single_seq_ECs(train_file)
-#retrive_esm1b_embedding(train_fasta_file)
-
-#compute_esm_distance(train_file)
-
+test_data_price = "price"
+test_data_new = "new"
+retrive_esm1b_embedding(test_data_new)
+retrive_esm1b_embedding(test_data_price)
 
 #esm2
 retrive_esm2_embedding(train_file)
