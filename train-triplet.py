89af7a1effafab73f517708f23691dfed15d99a1
biomg <40063814+biomg@users.noreply.github.com>
2024-07-12 16:30:36 +0800

    Update train-triplet.py

--- train-triplet.py
+++ train-triplet.py
@@ -5,9 +5,16 @@ import pickle
 from CLEAN.dataloader import *
 from CLEAN.model import *
 from CLEAN.utils import *
+from CLEAN.infer import *
 import torch.nn as nn
 import argparse
 from CLEAN.distance_map import get_dist_map
+import csv
+import pandas as pd
+import torch, gc
+from torch.cuda.amp import autocast
+
+
 
 
 def parse():
@@ -18,7 +25,7 @@ def parse():
     parser.add_argument('-t', '--training_data', type=str, default='split10')
     parser.add_argument('-d', '--hidden_dim', type=int, default=512)
     parser.add_argument('-o', '--out_dim', type=int, default=128)
-    parser.add_argument('--adaptive_rate', type=int, default=500)
+    parser.add_argument('--adaptive_rate', type=int, default=100)
     parser.add_argument('--verbose', type=bool, default=False)
     args = parser.parse_args()
     return args
@@ -26,6 +33,7 @@ def parse():
 
 def get_dataloader(dist_map, id_ec, ec_id, args):
     params = {
+        #'batch_size': 6000,
         'batch_size': 6000,
         'shuffle': True,
     }
@@ -44,11 +52,13 @@ def train(model, args, epoch, train_loader,
     for batch, data in enumerate(train_loader):
         optimizer.zero_grad()
         anchor, positive, negative = data
-        anchor_out = model(anchor.to(device=device, dtype=dtype))
-        positive_out = model(positive.to(device=device, dtype=dtype))
-        negative_out = model(negative.to(device=device, dtype=dtype))
+        #print(anchor.shape)
+        with autocast():
+            anchor_out = model(anchor.to(device=device, dtype=dtype))
+            positive_out = model(positive.to(device=device, dtype=dtype))
+            negative_out = model(negative.to(device=device, dtype=dtype))
 
-        loss = criterion(anchor_out, positive_out, negative_out)
+            loss = criterion(anchor_out, positive_out, negative_out)
         loss.backward()
         optimizer.step()
 
@@ -64,8 +74,69 @@ def train(model, args, epoch, train_loader,
     # record running average training loss
     return total_loss/(batch + 1)
 
+def createCSV():
+    
+    f_new_pvalue = open('./results_esm2/cnn3_new_pvalue.csv', mode='a', encoding='utf-8', newline='')
+    new_pvalue_writer= csv.DictWriter(f_new_pvalue, fieldnames=['epoch-new-pvalue', 'precision', 'recall', 'F1', 'AUC', 'accuracy'])
+    new_pvalue_writer.writeheader()
+    oo = {'epoch-new-pvalue':'cnn3', 'precision':'cnn3', 'recall': 'cnn3', 'F1': 'cnn3','AUC': 'cnn3', 'accuracy':'cnn3'}
+    new_pvalue_writer.writerow(oo)
+    
+    
+    f_new_maxsep = open('./results_esm2/cnn3_new_maxsep.csv', mode='a', encoding='utf-8', newline='')
+    new_maxsep_writer= csv.DictWriter(f_new_maxsep, fieldnames=['epoch-new-maxsep', 'precision', 'recall', 'F1', 'AUC', 'accuracy'])
+    new_maxsep_writer.writeheader()
+    oo = {'epoch-new-maxsep':'cnn3', 'precision':'cnn3', 'recall': 'cnn3', 'F1': 'cnn3','AUC': 'cnn3', 'accuracy':'cnn3'}
+    new_maxsep_writer.writerow(oo)
+    
+    
+    
+    f_price_pvalue = open('./results_esm2/cnn3_price_pvalue.csv', mode='a', encoding='utf-8', newline='')
+    price_pvalue_writer= csv.DictWriter(f_price_pvalue, fieldnames=['epoch-price-pvalue', 'precision', 'recall', 'F1', 'AUC', 'accuracy'])
+    price_pvalue_writer.writeheader()
+    oo = {'epoch-price-pvalue':'cnn3', 'precision':'cnn3', 'recall': 'cnn3', 'F1': 'cnn3','AUC': 'cnn3', 'accuracy':'cnn3'}
+    price_pvalue_writer.writerow(oo)
+    
+    
+    
+    f_price_maxsep = open('./results_esm2/cnn3_price_maxsep.csv', mode='a', encoding='utf-8', newline='')
+    price_maxsep_writer= csv.DictWriter(f_price_maxsep, fieldnames=['epoch-price-maxsep', 'precision', 'recall', 'F1', 'AUC', 'accuracy'])
+    price_maxsep_writer.writeheader()
+    oo = {'epoch-price-maxsep':'cnn3', 'precision':'cnn3', 'recall': 'cnn3', 'F1': 'cnn3','AUC': 'cnn3', 'accuracy':'cnn3'}
+    price_maxsep_writer.writerow(oo)
+   
+    
+def inferenceWirte(epoch, model):
+    
+    train_data = "split100"
+    test_data_price = "price"
+    test_data_new = "new"
+    
+    pre, rec, f1, auc, acc = infer_pvalue(train_data, test_data_new, p_value=1e-5, nk_random=20, report_metrics=True, pretrained=False, model_name=model)
+    new_pvalue = [epoch, round(pre, 4), round(rec, 4), round(f1, 4), round(auc, 4), round(acc, 4)]
+    print(new_pvalue)
+    pd.DataFrame([new_pvalue]).to_csv('./results_esm2/cnn3_new_pvalue.csv',mode='a', header=False, index=False)
+    
+    pre, rec, f1, auc, acc = infer_maxsep(train_data, test_data_new, report_metrics=True, pretrained=False, model_name=model)
+    new_maxsep = [epoch, round(pre, 4), round(rec, 4), round(f1, 4), round(auc, 4), round(acc, 4)]
+    print(new_maxsep)
+    pd.DataFrame([new_maxsep]).to_csv('./results_esm2/cnn3_new_maxsep.csv',mode='a', header=False, index=False)
+    
+    pre, rec, f1, auc, acc = infer_pvalue(train_data, test_data_price, p_value=1e-5, nk_random=20, report_metrics=True, pretrained=False, model_name=model)
+    price_pvalue = [epoch, round(pre, 4), round(rec, 4), round(f1, 4), round(auc, 4), round(acc, 4)]
+    print(price_pvalue)    
+    pd.DataFrame([price_pvalue]).to_csv('./results_esm2/cnn3_price_pvalue.csv',mode='a', header=False, index=False)
+ 
+        
+    pre, rec, f1, auc, acc = infer_maxsep(train_data, test_data_price, report_metrics=True, pretrained=False, model_name=model)
+    price_maxsep = [epoch, round(pre, 4), round(rec, 4), round(f1, 4), round(auc, 4), round(acc, 4)]
+    print(price_maxsep)
+    pd.DataFrame([price_maxsep]).to_csv('./results_esm2/cnn3_price_maxsep.csv',mode='a', header=False, index=False)
 
 def main():
+    #gc.collect()
+    #torch.cuda.empty_cache()
+    createCSV()
     seed_everything()
     ensure_dirs('./data/model')
     args = parse()
@@ -86,55 +157,48 @@ def main():
     esm_emb = pickle.load(
         open('./data/distance_map/' + args.training_data + '_esm.pkl',
                 'rb')).to(device=device, dtype=dtype)
+                
+    print(esm_emb)
+    print(esm_emb.shape)
     dist_map = pickle.load(open('./data/distance_map/' + \
         args.training_data + '.pkl', 'rb')) 
+    #print(dist_map)
+    #print(dist_map.shape)
     #======================== initialize model =================#
-    model = LayerNormNet(args.hidden_dim, args.out_dim, device, dtype)
+    model = CNN3(args.hidden_dim, args.out_dim, device, dtype)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999))
     criterion = nn.TripletMarginLoss(margin=1, reduction='mean')
     best_loss = float('inf')
     train_loader = get_dataloader(dist_map, id_ec, ec_id, args)
+    
+    
     print("The number of unique EC numbers: ", len(dist_map.keys()))
     #======================== training =======-=================#
-    # training
-    for epoch in range(0, epochs):
-        if epoch % args.adaptive_rate == 0 and epoch != epochs:
-            optimizer = torch.optim.Adam(
-                model.parameters(), lr=lr, betas=(0.9, 0.999))
-            # save updated model
-            torch.save(model.state_dict(), './data/model/' +
-                       model_name + '_' + str(epoch) + '.pth')
-            # delete last model checkpoint
-            '''
-            if epoch != args.adaptive_rate:
-                os.remove('./data/model/' + model_name + '_' +
-                          str(epoch-args.adaptive_rate) + '.pth')
-            '''
-            # sample new distance map
-            dist_map = get_dist_map(
-                ec_id_dict, esm_emb, device, dtype, model=model)
-            train_loader = get_dataloader(dist_map, id_ec, ec_id, args)
-        # -------------------------------------------------------------------- #
+    # training             
+    for epoch in range(1, epochs+1):
         epoch_start_time = time.time()
         train_loss = train(model, args, epoch, train_loader,
                            optimizer, device, dtype, criterion)
-        # only save the current best model near the end of training
-        if (train_loss < best_loss and epoch > 0.8*epochs):
-            torch.save(model.state_dict(), './data/model/' + model_name + '.pth')
-            best_loss = train_loss
-            print(f'Best from epoch : {epoch:3d}; loss: {train_loss:6.4f}')
-
+        
         elapsed = time.time() - epoch_start_time
         print('-' * 75)
         print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
-              f'training loss {train_loss:6.4f}')
+              f'training loss {train_loss:6.10f}')
         print('-' * 75)
-    # remove tmp save weights
-    #os.remove('./data/model/' + model_name + '.pth')
-    #os.remove('./data/model/' + model_name + '_' + str(epoch) + '.pth')
-    # save final weights
-    torch.save(model.state_dict(), './data/model/' + model_name + '.pth')
-
+        
+        #save a model every 1000 epoch after 4000 epoch
+        if epoch % 1000 == 0 and epoch > 0:
+            torch.save(model.state_dict(), './data/model/' + model_name + '_' + str(epoch) + '.pth')
+            inferenceWirte(epoch, model)
+            
+        if epoch % args.adaptive_rate == 0 and epoch != epochs:
+            optimizer = torch.optim.Adam(
+                model.parameters(), lr=lr, betas=(0.9, 0.999))
+            
+            # sample new distance map
+            dist_map = get_dist_map(ec_id_dict, esm_emb, device, dtype, model=model)
+            train_loader = get_dataloader(dist_map, id_ec, ec_id, args)
+   
 
 if __name__ == '__main__':
     main()
